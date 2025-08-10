b7e96e39c18f06eaac8aa0fcabd9d8f2c4a79ea4
sebasster <sebasster@gmail.com>
2025-08-10 02:36:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-// index.js
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Ruta principal
-app.get("/", (req, res) => {
-  res.send("🎵 Hola Seba! Tu backend está vivo y listo para rockear 🎸");
-});
-
-// Iniciar servidor
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en puerto ${PORT}`);
-});
